f223a5065268f78cac588853f6795a2f328d5ca8
EvgHrn <evg.hrn@gmail.com>
2020-10-01 08:31:42 +0400

    Get push messages permissions

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { messaging } from "./init-fcm";
+
 function App() {
+
+  const [text, setText] = React.useState("");
+
+  React.useEffect(() => {
+    const getPermission = async () => {
+      messaging.requestPermission()
+        .then(async function() {
+          const token = await messaging.getToken();
+          console.log("token: ", token);
+        })
+        .catch(function(err) {
+          console.log("Unable to get permission to notify.", err);
+        });
+    };
+    getPermission();
+    navigator.serviceWorker.addEventListener("message", (message) => console.log(message));
+  }, []);
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          With firebase Edit <code>src/App.tsx</code> and save to reload.
+          {text}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- src/init-fcm.tsx
+++ src/init-fcm.tsx
@@ -2,7 +2,13 @@ import * as firebase from "firebase/app";
 import "firebase/messaging";
 const initializedFirebaseApp = firebase.initializeApp({
   // Project Settings => Add Firebase to your web app
-  messagingSenderId: "1234567890",
+  apiKey: "AIzaSyB1GlGJi1naoccuhakNoF53d9PUhtkynqc",
+  authDomain: "pushtest-5b8c5.firebaseapp.com",
+  databaseURL: "https://pushtest-5b8c5.firebaseio.com",
+  projectId: "pushtest-5b8c5",
+  storageBucket: "pushtest-5b8c5.appspot.com",
+  messagingSenderId: "1040037119059",
+  appId: "1:1040037119059:web:581ef0f66b58dfd372f2a6",
 });
 const messaging = initializedFirebaseApp.messaging();
 export { messaging };
